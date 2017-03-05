bf0d0187eef2e1ac546b062fe69278c37d28d7d5
sanae <sanae.hadachi@gmail.com>
2017-03-05 12:26:25 +0100

    Added second sysout
    Change-Id: Ibe2967d7e7ebf79153ca8b9050c578906020fce0
    Signed-off-by: sanae <sanae.hadachi@gmail.com>

--- src/com/git/GitInEclipse.java
+++ src/com/git/GitInEclipse.java
@@ -8,7 +8,7 @@ public class GitInEclipse {
 	//this the main method
 	public static void main(String[] args) {
 		System.out.println("This is the first modification");
-
+		System.out.println("This is the second modification");
 	}
 
 }
