ef7af2881d548c1d1984b90d45cc063dbf911c8f
sanae <sanae.hadachi@gmail.com>
2017-03-05 11:35:09 +0100

    Added the first comment
    Change-Id: I35170f5ef8e6190e8895a84d1baa10e49a17c1f0
    Signed-off-by: sanae <sanae.hadachi@gmail.com>

--- src/com/git/GitInEclipse.java
+++ src/com/git/GitInEclipse.java
@@ -5,7 +5,7 @@ public class GitInEclipse {
 	public GitInEclipse() {
 		// TODO Auto-generated constructor stub
 	}
-
+	//this the main method
 	public static void main(String[] args) {
 		System.out.println("This is the first modification");
 
